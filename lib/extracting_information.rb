a5bd6f170adc4827225d0e9d09cafc0e2f693983
Patrick Harris <patricknpharris@gmail.com>
2018-09-04 11:58:44 +0100

    quick open file method created

--- lib/extracting_information.rb
+++ lib/extracting_information.rb
@@ -1,3 +1,9 @@
 def stringify(address)
   address
 end
+
+def openFile
+  File.open("../house_prices_by_area.rb", mode="r")
+end
+
+openFile
--- spec/extracting_information_spec.rb
+++ spec/extracting_information_spec.rb
@@ -1,7 +1,11 @@
 require 'extracting_information'
 
 describe 'stringify' do
-  it "Expects output to be returned as string." do
+  it 'Expects output to be returned as string.' do
     expect(stringify('address')).to match(String)
   end
+end
+
+describe 'openFile' do
+  it 'Opens the house prices file'
 end
